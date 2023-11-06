7f652a145be01492c67c4c7db1397db908a9ad6c
Duck-jun99 <byoungjun320@gmail.com>
2023-11-07 02:11:53 +0900

    feat:영양소분석기능 추가

--- app/src/main/java/com/mobilelec/dietms/view/DetailActivity.java
+++ app/src/main/java/com/mobilelec/dietms/view/DetailActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.mobilelec.dietms.R;
@@ -40,6 +41,7 @@ public class DetailActivity extends AppCompatActivity {
     TextView nutr8;
     TextView nutr9;
     ImageView imgDetail;
+    TextView info;
 
 
     @Override
@@ -60,9 +62,13 @@ public class DetailActivity extends AppCompatActivity {
         String createdDate = intent.getStringExtra("created_date");
         //String publishedDate = intent.getStringExtra("published_date");
         String image = intent.getStringExtra("image");
+        //HtmlCompat.fromHtml("{createdDate}에 대한 식단을 알려줄게요");
+        //"<b>${response.userInfo.nickname}님</b>, 환영해요!<br>오늘은 이 글 어때요?", HtmlCompat.FROM_HTML_MODE_LEGACY
+        String Htmldate = "<p><strong>"+createdDate+"</strong> 의 식단</p>";
+        String Htmlfood = "<p><strong>"+text+"</strong> 에 대한 영양정보 입니다.</p>";
 
-        dateDetail.setText(createdDate);
-        foodDetail.setText(text);
+        dateDetail.setText(HtmlCompat.fromHtml(Htmldate,HtmlCompat.FROM_HTML_MODE_COMPACT));
+        foodDetail.setText(HtmlCompat.fromHtml(Htmlfood,HtmlCompat.FROM_HTML_MODE_COMPACT));
         Picasso.get().load(image).into(imgDetail);
 
 
@@ -84,6 +90,7 @@ public class DetailActivity extends AppCompatActivity {
         nutr7 = findViewById(R.id.nutr7);
         nutr8 = findViewById(R.id.nutr8);
         nutr9 = findViewById(R.id.nutr9);
+        info = findViewById(R.id.info);
     }
 
     private class NetworkRequestTask extends AsyncTask<Void, Void, String> {
@@ -133,16 +140,61 @@ public class DetailActivity extends AppCompatActivity {
                     nutr8.setText("포화지방: " + firstObject.getString("NUTR_CONT8") + " g");
                     nutr9.setText("트랜스지방: " + firstObject.getString("NUTR_CONT9") + " g");
 
+
+                    float nutrCont1Float;
+                    float nutrCont6Float;
+                    try {
+                        nutrCont1Float = Float.parseFloat(firstObject.getString("NUTR_CONT1"));
+                        nutrCont6Float = Float.parseFloat(firstObject.getString("NUTR_CONT6"));
+                    } catch (NumberFormatException e) {
+                        nutrCont1Float = 0;
+                        nutrCont6Float = 0;
+                    } catch (JSONException e) {
+                        nutrCont1Float = 0;
+                        nutrCont6Float = 0;
+                    }
+
+                    float male = 2400 - nutrCont1Float;
+                    float female = 2000 - nutrCont1Float;
+                    String and = "또한, \n";
+                    String text1 = "사용자님의 식단은 "+firstObject.getString("NUTR_CONT1") + " kcal 이상입니다.\n";
+                    String text2 = "나트륨 함량이 "+firstObject.getString("NUTR_CONT6") + " mg 이상입니다.\n";
+                    String text1Warnig = "\n";
+                    String text2Warnig = "\n";
+
+                    if(nutrCont1Float > 500){
+
+                        text1Warnig = "고열량의 음식을 섭취하신 만큼, 남은 끼니에 대한 각별한 주의가 필요합니다.\n";
+                    }
+
+                    if(nutrCont6Float > 450 && nutrCont6Float<900){
+
+                        text2Warnig = "나트륨 섭취에 주의가 필요합니다.\n";
+                    }
+                    else if(nutrCont6Float >= 900){
+                        text2Warnig = "나트륨 함량이 매우 높은 음식이므로,\n" +
+                                "음식 섭취에 각별한 주의가 필요합니다.\n";
+                    }
+
+                    info.setText(text1 +
+                            text1Warnig +
+                            "남은 하루 필요 칼로리는 아래와 같습니다. \n" +
+                            "- 남성이신 경우 " + male + " kcal,\n" +
+                            "- 여성이신 경우 " + female + " kcal\n" +
+                            and + text2 + text2Warnig +
+                            "이 점을 참고하여 건강한 식사하시길 바랍니다.\n" +
+                            "아래에서 세부사항을 확인해주세요.\n");
+
                     /*
-                    13	NUTR_CONT1	열량(kcal)(1회제공량당)
-                    14	NUTR_CONT2	탄수화물(g)(1회제공량당)
-                    15	NUTR_CONT3	단백질(g)(1회제공량당)
-                    16	NUTR_CONT4	지방(g)(1회제공량당)
-                    17	NUTR_CONT5	당류(g)(1회제공량당)
-                    18	NUTR_CONT6	나트륨(mg)(1회제공량당)
-                    19	NUTR_CONT7	콜레스테롤(mg)(1회제공량당)
-                    20	NUTR_CONT8	포화지방산(g)(1회제공량당)
-                    21	NUTR_CONT9	트랜스지방(g)(1회제공량당)
+                    NUTR_CONT1	열량(kcal)(1회제공량당)
+                    NUTR_CONT2	탄수화물(g)(1회제공량당)
+                    NUTR_CONT3	단백질(g)(1회제공량당)
+                    NUTR_CONT4	지방(g)(1회제공량당)
+                    NUTR_CONT5	당류(g)(1회제공량당)
+                    NUTR_CONT6	나트륨(mg)(1회제공량당)
+                    NUTR_CONT7	콜레스테롤(mg)(1회제공량당)
+                    NUTR_CONT8	포화지방산(g)(1회제공량당)
+                    NUTR_CONT9	트랜스지방(g)(1회제공량당)
                      */
 
                 } catch (JSONException e) {
